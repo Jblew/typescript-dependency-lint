d40d9978afe27fdab72f8a8c3993de1a67a68d0f
Jędrzej Lewandowski <jedrzej@pitchup.com>
2020-04-15 19:22:23 +0000

    feat: local resolver

--- src/analyze/resolveImport.ts
+++ src/analyze/resolveImport.ts
@@ -1,22 +1,51 @@
+import * as fs from 'fs'
+import * as paths from 'path'
 import { createMatchPath } from 'tsconfig-paths'
 
 import { TSConfigBaseStructure } from '../util/TSConfigBaseStructure'
 
-export function resolveImport({
-  tsconfig,
-  importPath,
-  contextPath,
-}: {
+export function resolveImport(params: Params): string | undefined {
+  const resolvedLocal = resolveLocal(params)
+  if (resolvedLocal) return resolvedLocal
+
+  const resolvedWithTsconfig = resolveWithTsconfigPaths(params)
+  if (resolvedWithTsconfig) return resolvedWithTsconfig
+
+  if (isLibraryImport(params)) return params.importPath
+
+  throw new Error(
+    `Cannot resolve import ` +
+      `'${params.importPath}' in '${params.contextPath}'`,
+  )
+}
+
+interface Params {
   tsconfig: TSConfigBaseStructure
   importPath: string
   contextPath: string
-}): string | undefined {
+}
+
+function resolveLocal({ importPath, contextPath }: Params): string | undefined {
+  const path = paths.resolve(paths.resolve(importPath, contextPath))
+  if (fs.existsSync(path)) return path
+  return undefined
+}
+
+function resolveWithTsconfigPaths({
+  tsconfig,
+  importPath,
+  contextPath,
+}: Params): string | undefined {
   const matcher = createMatchPath(
     tsconfig.absoluteBaseUrl,
     tsconfig.compilerOptions.paths,
   )
-  const matched = matcher(importPath)
-  // tslint:disable no-console
-  console.log({ tsconfig, importPath })
-  return matched
+  const resolvedPath = matcher(importPath)
+
+  return resolvedPath
+}
+
+function isLibraryImport(_: Params): boolean {
+  // for now. Later we may consider resolving with package.json
+  return true
 }
